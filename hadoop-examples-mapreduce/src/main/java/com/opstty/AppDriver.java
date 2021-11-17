42fc770e8a2750fb0e7fbfffc821b5ff347fe024
arcticvertigo <iris.hubert@efrei.net>
2021-11-17 22:56:26 +0100

    correct bug

--- hadoop-examples-mapreduce/src/main/java/com/opstty/AppDriver.java
+++ hadoop-examples-mapreduce/src/main/java/com/opstty/AppDriver.java
@@ -2,6 +2,12 @@ package com.opstty;
 
 import com.opstty.job.WordCount;
 import org.apache.hadoop.util.ProgramDriver;
+import com.opstty.job.DDistricts;
+import com.opstty.job.MaxHeightSpecies;
+import com.opstty.job.TreeSpecies;
+import com.opstty.job.TreeSpeciesCount;
+import com.opstty.job.TreesSortedByHeight;
+
 
 public class AppDriver {
     public static void main(String argv[]) {
@@ -11,6 +17,21 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
+
+            programDriver.addClass("distinctDistricts", DDistricts.class,
+                    "A map/reduce program that returns the distinct districts with trees in the Remarkable Trees of Paris dataset.");
+
+            programDriver.addClass("treeSpecies", TreeSpecies.class,
+                    "A map/reduce program that returns the distinct tree species in the Remarkable Trees of Paris dataset.");
+
+            programDriver.addClass("treeSpeciesCount", TreeSpeciesCount.class,
+                    "A map/reduce program that returns the distinct tree species (and the number of trees for each one) in the Remarkable Trees of Paris dataset.");
+
+            programDriver.addClass("maxHeightSpecies", MaxHeightSpecies.class,
+                    "A map/reduce program that returns the highest height of trees per species in the Remarkable Trees of Paris dataset.");
+
+            programDriver.addClass("treesSortedByHeight", TreesSortedByHeight.class,
+                    "A map/reduce program that returns all the trees in the Remarkable Trees of Paris dataset, sorted by height.");
 
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
